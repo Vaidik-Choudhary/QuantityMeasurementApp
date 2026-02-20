31de3aec4c05101a008fdd5539e8b4a168fb06a9
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-02-20 17:05:29 +0530

    [Vaidik choudhary] : Added QuantityMeasurementApp.java file

---
+++ src/main/java/com/quantitymeasurementapp/QuantityMeasurementApp.java
@@ -0,0 +1,19 @@
+package com.quantitymeasurementapp;
+
+import java.util.Scanner;
+
+public class QuantityMeasurementApp {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Number : ");
+		double inputOne = sc.nextDouble();
+		System.out.println("Enter Second Number : ");
+		double inputTwo = sc.nextDouble();
+		Feet f1 = new Feet(inputOne);
+        Feet f2 = new Feet(inputTwo);
+
+        System.out.println("Are equal? " + f1.equals(f2));
+        sc.close();
+	}
+}
