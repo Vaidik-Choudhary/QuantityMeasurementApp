8fcc50f60206b176e63816175a8632d014f1dac1
Vaidik Choudhary <vaidikchoudhary771@gmail.com>
2026-02-24 11:45:15 +0530

    [Vaidik choudhary] : Updated IMeasurable.java file

---
+++ src/main/java/com/quantitymeasurementapp/IMeasurable.java
@@ -0,0 +1,12 @@
+package com.quantitymeasurementapp;
+
+public interface IMeasurable {
+
+	public double getConversionFactor();
+
+	public double convertToBaseUnit(double value);
+
+	public double convertFromBaseUnit(double baseValue);
+
+	public String getUnitName();
+}
